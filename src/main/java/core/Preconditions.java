a785d02759f8dfae193934af7b98f783f84249a3
Eugene911111 <golubeugene@rambler.ru>
2017-05-24 14:16:16 +0300

    stable

---
+++ src/main/java/core/Preconditions.java
@@ -0,0 +1,24 @@
+package core;
+
+import pages.CommonPage;
+import pages.LoginPage;
+
+import static core.Configuration.USER_FORMS_STATUS_IN_PROGRESS;
+import static helpers.SqlQueries.*;
+
+public class Preconditions {
+    private CommonPage commonPage = new CommonPage();
+    private LoginPage loginPage = new LoginPage();
+
+    public void precondition(String userName) throws Exception {
+        changeStatusOfUserForm(USER_FORMS_STATUS_IN_PROGRESS);
+        delete1(DELETE_USER_BY_ID);
+        deleteGoalById(DELETE_GOALS_BY_ID);
+        loginPage.logIn(userName);
+    }
+
+    public void postcondition() throws Exception {
+        commonPage.logOut();
+    }
+}
+
